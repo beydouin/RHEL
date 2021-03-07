c229bdc9c0127cc6a4b5a45f6bcbc1dd8f44d700
Khurram <khurramsubhani@gmail.com>
2021-03-07 12:43:58 -0500

    modified the script to add functions

--- RHEL_BASH_SCRIPTS/RHELMajorMinorVersion.sh
+++ RHEL_BASH_SCRIPTS/RHELMajorMinorVersion.sh
@@ -1,14 +1,38 @@
 #!/usr/bin/env bash
-# Demonstrates grabbing major and minor version of RHEL
+# 03/07/2021
+# khurram.subhani@live.com
+# This demonstrates several bash features:
+# 1) global variables
+# 2) local variables
+# 3) setting global variables from within function
+# 4) if else and nested if else statements
 
-RHEL_RELEASE="$(cat /etc/redhat-release | grep -Eo '?[0-9]+[.]+[0-9]?')"
-major_version="$(cat $RHEL_RELEASE | awk -F'.' '{print $1}')"
-minor_version="$(cat $RHEL_RELEASE | awk -F'.' '{print $2}')"
+RHEL_RELEASE=""
+major_version=""
+minor_version=""
 
-if [ $major_version -ge 7 ]; then
-    echo This version of RHEL is $major_version
-elif [ $major_version -le 7 ]; then
-    echo This version of RHEL is $major_version
-else
-    echo couldn\'t find $major_version
-fi
+checkRHELVersion() {
+    RHEL_RELEASE="$(cat /etc/redhat-release | grep -Eo '?[0-9]+[.]+[0-9]?')"
+    if [ $? != 0 ]; then
+        RHEL_RELEASE=1
+    fi
+    major_version="$(cat $RHEL_RELEASE | awk -F'.' '{print $1}')"
+    minor_version="$(cat $RHEL_RELEASE | awk -F'.' '{print $2}')"
+}
+
+main() {
+    checkRHELVersion
+    if [ $RHEL_RELEASE -eq 1 ]; then
+        echo This is not a RHEL system...
+    else
+        if [ $major_version -ge 7 ]; then
+            echo This is RHEL version $major_version, which is greater than or equal to 7
+        fi
+
+        if [ $major_version -lt 7 ]; then
+            echo This RHEL version $major_version is less than version 7
+        fi
+    fi 
+}
+
+main
